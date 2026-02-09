4361feadbfad9d99f82368733bcfaeb71ac44082
Madan <29ramadhanirachman@gmail.com>
2026-02-09 11:03:30 +0700

    no4

--- 04-exercise.ts
+++ 04-exercise.ts
@@ -1,8 +1,19 @@
-function maxProfit(days: any[]) {
-  let day = 0;
+function maxProfit(prices: any[]) {
+  let buyPrices = prices[0];
+  let highest = 0;
 
-  for (let i = 0; i < days.length; i++) {
-    if (days[i] === 0) {
+  for (let i = 0; i < prices.length; i++) {
+    // apabila harga hari ini lebih murah dari sebelumnya maka ubah beli di harga termurah saat ini
+    if (prices[i] < buyPrices) {
+      buyPrices = prices[i];
+    } else {
+      const profit = prices[i] - buyPrices;
+      if (profit > highest) {
+        highest = profit;
+      }
     }
   }
+  return highest;
 }
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
